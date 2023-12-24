7cc5d8d2d5ce54fd3a79c3ae3d7ee581563656cd
Gražvydas Žilius <grazvydaszilius2001@gmail.com>
2023-12-24 11:03:22 +0200

    Updated pizza project directory

---
+++ 03-pizza-menu/starter/my-app/src/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Ughhh
+      </header>
+    </div>
+  );
+}
+
+export default App;
